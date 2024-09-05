a5c767144c97d7230d66e28729647e7b7eebf0fc
ADMIN <michellelallydev>
2024-09-05 08:50:56 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-700 text-white py-4">
+      <div className="container mx-auto flex justify-center items-center">
+        <div className="text-xl font-bold">Image Recognition App</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
